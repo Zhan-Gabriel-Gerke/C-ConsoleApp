f9482554a9192210d7e179e6c6c0935e95bbf47a
Zhan-Gabriel Gerke <zangerke3@gmail.com>
2025-04-23 15:18:56 +0300

    Homework has done

--- C#ConsoleApp/MainClass.cs
+++ C#ConsoleApp/MainClass.cs
@@ -4,27 +4,53 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO; // Added for File and Path classes
 //"-Текст
 //'-Символ
+
 namespace C_ConsoleApp
 {
     internal class MainClass
     {
-
         public static void Main(string[] args)
         {
+            List<string> kuude_list = new List<string>();
             try
             {
-                string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Kuud.txt"); //@"..\..\..\Kuud.txt"
-                StreamWriter text = new StreamWriter(path, true); // true = Добавляет к имя к тому что уже есть
-                Console.WriteLine("Sisesta mingi tekst: ");
-                string lause = Console.ReadLine();
-                text.WriteLine(lause);
-                text.Close();
+                string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Kuud.txt");
+                kuude_list.Add("April");
+                kuude_list.Add("September");
+                kuude_list.Add("Juuni");
+                File.WriteAllLines(path, kuude_list);
+                Console.WriteLine("OK");
+                kuude_list.Clear();
+                foreach (string rida in File.ReadAllLines(path))
+                {
+                    kuude_list.Add(rida);
+                }
+                foreach (string kuu in kuude_list)
+                {
+                    Console.WriteLine(kuu);
+                }
+                kuude_list.Remove("Juuni");
+                kuude_list[0] = "TEST kuuu";
+                foreach (string kuu in kuude_list)
+                {
+                    Console.WriteLine(kuu);
+                }
+                Console.WriteLine("Sisesta kuu nimi, mida otsida:");
+                string otsitav = Console.ReadLine();
+
+                if (kuude_list.Contains(otsitav))
+                    Console.WriteLine("Kuu " + otsitav + " on olemas.");
+                else
+                    Console.WriteLine("Sellist kuud pole.");
+                File.WriteAllLines(path, kuude_list);
+
             }
             catch (Exception)
             {
-                Console.WriteLine("Mingi viga failiga");
+                Console.WriteLine("Viga failiga!");
             }
         }
     }
