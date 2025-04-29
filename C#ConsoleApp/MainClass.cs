f1d2b1bf9f229641cb71d6bdd838bc1034feee40
Zhan-Gabriel Gerke <zangerke3@gmail.com>
2025-04-29 21:17:44 +0300

    save

--- C#ConsoleApp/MainClass.cs
+++ C#ConsoleApp/MainClass.cs
@@ -14,44 +14,6 @@ namespace C_ConsoleApp
     {
         public static void Main(string[] args)
         {
-            List<string> kuude_list = new List<string>();
-            try
-            {
-                string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Kuud.txt");
-                kuude_list.Add("April");
-                kuude_list.Add("September");
-                kuude_list.Add("Juuni");
-                File.WriteAllLines(path, kuude_list);
-                Console.WriteLine("OK");
-                kuude_list.Clear();
-                foreach (string rida in File.ReadAllLines(path))
-                {
-                    kuude_list.Add(rida);
-                }
-                foreach (string kuu in kuude_list)
-                {
-                    Console.WriteLine(kuu);
-                }
-                kuude_list.Remove("Juuni");
-                kuude_list[0] = "TEST kuuu";
-                foreach (string kuu in kuude_list)
-                {
-                    Console.WriteLine(kuu);
-                }
-                Console.WriteLine("Sisesta kuu nimi, mida otsida:");
-                string otsitav = Console.ReadLine();
-
-                if (kuude_list.Contains(otsitav))
-                    Console.WriteLine("Kuu " + otsitav + " on olemas.");
-                else
-                    Console.WriteLine("Sellist kuud pole.");
-                File.WriteAllLines(path, kuude_list);
-
-            }
-            catch (Exception)
-            {
-                Console.WriteLine("Viga failiga!");
-            }
         }
     }
 }
--- C#ConsoleApp/Opilane.cs
+++ C#ConsoleApp/Opilane.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace C_ConsoleApp
 {
-    class Class1
+    class Opilane
     {
     }
 }
