3c4446d987d26c03e04f5405359a5661bd8d3919
Zhan-Gabriel Gerke <zangerke3@gmail.com>
2025-04-30 07:43:24 +0300

    Ex nr 3

--- C#ConsoleApp/MainClass.cs
+++ C#ConsoleApp/MainClass.cs
@@ -4,7 +4,9 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.IO; // Added for File and Path classes
+using System.IO;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates; // Added for File and Path classes
 //"-Текст
 //'-Символ
 
@@ -14,7 +16,56 @@ namespace C_ConsoleApp
     {
         public static void Main(string[] args)
         {
+            Opilane();
         }
-    }
+        public static void Opilane()
+        {
+            List<Opilane> Opilased = new List<Opilane>();
+            Opilane p1 = new Opilane();
+            p1.Name = "Zhan";
+            p1.Grades = new List<int>();
+            p1.Grades.Add(1);
+            p1.Grades.Add(2);
+            p1.Grades.Add(3);
+
+            Opilane p2 = new Opilane();
+            p2.Name = "Gabrile";
+            p2.Grades = new List<int>();
+            p2.Grades.Add(2);
+            p2.Grades.Add(3);
+            p2.Grades.Add(4);
+
+            Opilane p3 = new Opilane();
+            p3.Name = "Gerke";
+            p3.Grades = new List<int>();
+            p3.Grades.Add(5);
+            p3.Grades.Add(5);
+            p3.Grades.Add(5);
+
+            Opilased.Add(p1);
+            Opilased.Add(p2);
+            Opilased.Add(p3);
+            double theBest = 0;
+            string theBestName = ".";
+            foreach (Opilane p in Opilased)
+            {
+                double kesk = 0;
+                List<int> grades = p.Grades;
+                for (int i = 0; i < grades.Count; i++)
+                {
+                    kesk = kesk + grades[i];
+                }
+                kesk = Math.Round(kesk / grades.Count);
+                if (kesk > theBest)
+                {
+                    theBest = kesk;
+                    theBestName = p.Name;
+                }
+                Console.WriteLine($"Avg Grade {kesk} Name {p.Name}");
+            }
+            Console.WriteLine($"The best student {theBestName}, Avg grade {theBest}");
+
+        }
+    }   
 
 }
