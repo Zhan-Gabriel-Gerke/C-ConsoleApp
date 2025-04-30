ac964034fb15ccee0519205bbf9dcbc551becf91
Zhan-Gabriel Gerke <zangerke3@gmail.com>
2025-04-30 08:26:32 +0300

    Save

--- C#ConsoleApp/MainClass.cs
+++ C#ConsoleApp/MainClass.cs
@@ -16,8 +16,66 @@ namespace C_ConsoleApp
     {
         public static void Main(string[] args)
         {
-            Opilane();
+            //Opilane();(
+            AnimalsSystem();
         }
+        public static void AnimalsSystem()
+        {
+            List<Lemmikloom> Animals = new List<Lemmikloom>();
+            Lemmikloom a1 = new Lemmikloom();
+            a1.Name = "Bruno";
+            a1.Type = "Dog";
+            a1.Age = 3;
+
+            Lemmikloom a2 = new Lemmikloom();
+            a2.Name = "Karla";
+            a2.Type = "Cat";
+            a2.Age = 10;
+
+            Lemmikloom a3 = new Lemmikloom();
+            a3.Name = "Trickel";
+            a3.Type = "Dog";
+            a3.Age = 1;
+
+            Lemmikloom a4 = new Lemmikloom();
+            a4.Name = "Sharik";
+            a4.Type = "Dog";
+            a4.Age = 15;
+
+            Lemmikloom a5 = new Lemmikloom();
+            a5.Name = "Max";
+            a5.Type = "Dog";
+            a5.Age = 8;
+
+            Animals.Add (a1);
+            Animals.Add (a2);
+            Animals.Add (a3);
+            Animals.Add (a4);
+            Animals.Add (a5);
+            double theOldestAge = 0;
+            string theOldest = ".";
+            double kesk = 0;
+            foreach (Lemmikloom a in Animals)
+            {
+                if (a.Type == "Cat")
+                {
+                    Console.WriteLine($"Name {a.Name}, {a.Type}, Age {a.Age}");
+                }
+                for (int i = 0; i < Animals.Count; i++)
+                {
+                    kesk = kesk + a.Age;
+                }
+                kesk = Math.Round(kesk / Animals.Count);
+                if (a.Age > theOldestAge)
+                {
+                    theOldestAge = kesk;
+                    theOldest = a.Name;
+                }
+            }
+            Console.WriteLine($"The oldest animal it's {theOldest}, {theOldestAge} Year Old");
+            Console.WriteLine($"Avg age {kesk}");
+        }
+
         public static void Opilane()
         {
             List<Opilane> Opilased = new List<Opilane>();
